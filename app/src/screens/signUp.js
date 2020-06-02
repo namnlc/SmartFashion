676f98165deacdb26a3b9a364c1bcdbf337cacff
NamJaVer <38275210+16529769@users.noreply.github.com>
2020-06-03 00:18:02 +0700

    Complete ui signin, signup

--- app/src/screens/signUp.js
+++ app/src/screens/signUp.js
@@ -1,28 +1,29 @@
-// import React, {Component, useState} from 'react';
-// import {
-//   Text,
-//   View,
-//   Image,
-//   SafeAreaView,
-//   TextInput,
-//   StyleSheet,
-//   Dimensions,
-//   TouchableOpacity,
-// } from 'react-native';
- import top from '../../res/images/Signin/top.png';
- import logo from '../../res/images/Signin/logo.png';
- import Icon from 'react-native-vector-icons';
+import top from '../../res/images/Signin/top.png';
+import logo from '../../res/images/Signin/logo.png';
+import Icon from 'react-native-vector-icons';
 import tab from '../components/toggleModel';
- import printer from '../../res/images/Signin/printer.png';
- import facebook from '../../res/images/Signin/facebook.png';
- import twitter from '../../res/images/Signin/twitter.png';
- import instagram from '../../res/images/Signin/instagram.png';
- import google from '../../res/images/Signin/google.png';
- import touchid from '../../res/images/Signin/touchid.png'
- import Divider from 'react-native-divider';
- //import { TabView, SceneMap } from 'react-native-tab-view'
- //const {width} = Dimensions.get('window').width;
- //const initialLayout = { width: Dimensions.get('window').width };
+import printer from '../../res/images/Signin/printer.png';
+import facebook from '../../res/images/Signin/facebook.png';
+import twitter from '../../res/images/Signin/twitter.png';
+import instagram from '../../res/images/Signin/instagram.png';
+import google from '../../res/images/Signin/google.png';
+import touchid from '../../res/images/Signin/touchid.png';
+import Divider from 'react-native-divider';
+import Animated from 'react-native-reanimated';
+import * as React from 'react';
+import {
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  TextInput,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
+import {TabView, SceneMap} from 'react-native-tab-view';
+
+const initialLayout = {width: Dimensions.get('window').width};
 
 const RenderSignUp = () => {
   return (
@@ -66,9 +67,9 @@ const RenderSignUp = () => {
       </View>
     </View>
   );
-}
+};
 
-const  RenderSignIn = () => {
+const RenderSignIn = () => {
   return (
     <View style={styles.container}>
       <Text style={signin.txt}>
@@ -85,18 +86,14 @@ const  RenderSignIn = () => {
         placeholderTextColor="#696969"
         style={signup.txtInput}
       />
-      <TouchableOpacity style={{width:'80%',
-      flexDirection:'row-reverse',
-      }}>
-        <Text style={{color:'#098FA8', fontSize:12}}>
-          Forgot Password?
-        </Text>
+      <TouchableOpacity style={{width: '80%', flexDirection: 'row-reverse'}}>
+        <Text style={{color: '#098FA8', fontSize: 12}}>Forgot Password?</Text>
       </TouchableOpacity>
       <TouchableOpacity style={signup.button}>
         <Text style={signup.txtButton}>Get a code</Text>
       </TouchableOpacity>
       <TouchableOpacity style={signin.touchID}>
-        <Image source={touchid}/>
+        <Image source={touchid} />
         <Text style={signin.txtTouchId}>Touch ID</Text>
       </TouchableOpacity>
       <View style={signup.divider}>
@@ -113,34 +110,66 @@ const  RenderSignIn = () => {
       </View>
     </View>
   );
+};
+export default class SignUp extends React.Component {
+
+  state = {
+    index: 0,
+    routes: [{key: 'first', title: 'Sign In'}, {key: 'second', title: 'Sign Up'}],
+  };
+
+  _handleIndexChange = index => this.setState({index});
+
+  _renderTabBar = props => {
+    const inputRange = props.navigationState.routes.map((x, i) => i);
+    const index = props.nowindex;
+    return (
+      <View style={styles.tabContainer}>
+        <View >
+          <Image source={logo} style={{ top:8,left:-10}}/>
+        </View>
+        <View style={styles.tabView}>
+          {props.navigationState.routes.map((route, i) => {
+            let color = '#80BFCB';
+            if (this.state.index == i) {
+              color = '#098FA8';
+            }
+            let backgroundColor = 'transparent';
+            if (this.state.index == i) {
+              backgroundColor = '#fff';
+            }
+            return (
+              <TouchableOpacity
+                style={[styles.tabBar, {backgroundColor}]}
+                onPress={() => this.setState({index: i})}>
+                <Animated.Text style={[styles.txtTab,{color}]}>{route.title}</Animated.Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+        <View >
+          <Image source={logo} style={{ top:8,right:-10}}/>
+        </View>
+      </View>
+    );
+  };
+
+  _renderScene = SceneMap({
+    first: RenderSignIn,
+    second: RenderSignUp,
+  });
+
+  render() {
+    return (
+      <TabView
+        navigationState={this.state}
+        renderScene={this._renderScene}
+        renderTabBar={this._renderTabBar}
+        onIndexChange={this._handleIndexChange}
+      />
+    );
+  }
 }
-
-// function SignUp() {
-//   const [screen,setScreen] = React.useState(0);
-
-//   const [routes] = React.useState([
-//     { key: 'SignIn', title: 'name' },
-//     { key: 'SignUp', title: 'age' },
-//   ]);
-
-//   const renderScene = SceneMap({
-//     SignIn:RenderSignIn,
-//     SignUp:RenderSignUp,
-//   })
-
-//   // const handleToggle = ()=>{
-//   //   setScreen(prevState=>!prevState)
-//   // }
-//   return (
-//       <TabView 
-//        navigationState={{ screen, routes }}
-//        renderScene={renderScene}
-//        onIndexChange={setScreen}
-//        //initialLayout={initialLayout}
-//       />
-//   );
-// }
-
 
 const signup = StyleSheet.create({
   txt: {
@@ -193,25 +222,25 @@ const signin = StyleSheet.create({
     width: '80%',
     fontSize: 15,
     fontWeight: 'bold',
-    paddingBottom:30,
+    paddingBottom: 30,
     //letterSpacing:0.1
   },
-  txtTouchId:{
-    color:"#098FA8",
-    fontSize:15,
-    textAlign:'center',
-    fontWeight:'bold',
-    padding:10
+  txtTouchId: {
+    color: '#098FA8',
+    fontSize: 15,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    padding: 10,
   },
-  touchID:{
-    flexDirection:'row',
-    backgroundColor:'#E2F7FD',
-    width:'75%',
-    marginTop:10,
-    height:50,
+  touchID: {
+    flexDirection: 'row',
+    backgroundColor: '#E2F7FD',
+    width: '75%',
+    marginTop: 10,
+    height: 50,
     justifyContent: 'center',
-    alignItems:'center'
-  }
+    alignItems: 'center',
+  },
 });
 const styles = StyleSheet.create({
   container: {
@@ -231,51 +260,38 @@ const styles = StyleSheet.create({
     top: 30,
     left: 20,
   },
+
+  // render custom tabbar
+  tabContainer: {
+    alignItems: 'center',
+    flexDirection:'row',
+    padding: 20,
+    justifyContent: 'space-around',
+    backgroundColor: '#098FA8',
+    borderBottomLeftRadius: 28,
+  },
+  tabView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent:'center',
+    backgroundColor: '#06839A',
+    borderRadius: 10,
+    marginTop:20,
+    width:'65%',
+    height:50,
+  },
+  tabBar: {
+    //flexDirection:'row',
+    alignItems: 'center',
+    justifyContent:'center',
+    width: '43%',
+    padding: 10,
+    //marginRight:-20,
+    borderRadius: 10,   
+  },
+
+  txtTab:{
+    fontSize:14,
+    fontWeight:'bold'
+  }
 });
-
-// export default SignUp;
-
-import * as React from 'react';
-import {  Text,
-  View,
-  Image,
-  SafeAreaView,
-  TextInput,
-  StyleSheet,
-  Dimensions,
-  TouchableOpacity
- } from 'react-native';
-import { TabView, SceneMap } from 'react-native-tab-view';
-
-// const FirstRoute = () => (
-//   <View style={[styles.scene, { backgroundColor: '#ff4081' }]} />
-// );
-
-// const SecondRoute = () => (
-//   <View style={[styles.scene, { backgroundColor: '#673ab7' }]} />
-// );
-
-const initialLayout = { width: Dimensions.get('window').width };
-
-export default function TabViewExample() {
-  const [index, setIndex] = React.useState(0);
-  const [routes] = React.useState([
-    { key: 'first', title: 'First' },
-    { key: 'second', title: 'Second' },
-  ]);
-
-  const renderScene = SceneMap({
-    first: RenderSignIn,
-    second: RenderSignUp,
-  });
-
-  return (
-    <TabView
-      navigationState={{ index, routes }}
-      renderScene={renderScene}
-      onIndexChange={setIndex}
-      initialLayout={initialLayout}
-    />
-  );
-}
-
